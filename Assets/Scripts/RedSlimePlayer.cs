ba7b1e3046f756d866beb57e2de1ceef1f5d3677
Josh Malmquist <joshmalmquist96@gmail.com>
2016-11-11 21:55:29 -0500

    commited Player's slime scripts
    did stuff

---
+++ Assets/Scripts/RedSlimePlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RedSlimePlayer : PlayerSlime {
+    
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    protected override void SlimeAttack2()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void SlimeTrap()
+    {
+        throw new NotImplementedException();
+    }
+}
