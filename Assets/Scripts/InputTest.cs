086ef9d8f1ea120cd97703bb957153933190d6cd
Kevin Idzik <kdi4293@ad.rit.edu>
2016-11-11 22:13:32 -0500

    Input Mapping Updates

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetAxis("FireLeft") > 0.0f)
+        {
+            Debug.Log("Pew Left");
+        }
+        if (Input.GetAxis("FireRight") > 0.0f)
+        {
+            Debug.Log("Pew Right");
+        }
+        if (Input.GetAxis("Aim") != 0.0f)
+        {
+            Debug.Log(Input.GetAxis("Aim"));
+        }
+        if (Input.GetButtonDown("Trap"))
+        {
+            Debug.Log("Trap");
+        }
+        if (Input.GetButtonDown("Ultimate"))
+        {
+            Debug.Log("Ultimate");
+        }
+    }
+}
