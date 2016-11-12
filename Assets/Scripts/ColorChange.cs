04c8ea35f03ed572d3384685b873f415d2d1b984
Kevin Idzik <kdi4293@ad.rit.edu>
2016-11-12 16:19:11 -0500

    Health Updates

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChange : MonoBehaviour
+{
+    SpriteRenderer healthSR; //Health sprite renderer
+    float colorTimer; //Timer for the color
+
+	// Use this for initialization
+	void Start()
+    {
+        healthSR = GetComponent<SpriteRenderer>(); //Get the health's sprite renderer	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        colorTimer += (Time.deltaTime / 6); //Increment the color timer
+        healthSR.color = new Color(colorTimer, 0.0f, 0.0f); //Change the color of the health
+	}
+}
