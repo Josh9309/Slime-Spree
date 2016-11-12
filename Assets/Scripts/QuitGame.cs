8f3b46e48fe7e6529196d20a4eb9fbf3c1345a9a
satchpuri <sp1796@ad.rit.edu>
2016-11-12 00:31:47 -0500

    ui
    Made main menu

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+	//quit game
+    public void Quit()
+    {
+        //ends the scence
+#if UNITY_EDITOR
+           UnityEditor.EditorApplication.isPlaying = false;
+#else
+        //quits the exe
+        Application.Quit();
+#endif
+    }
+}
