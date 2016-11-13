07662c2b9a9b56f6555b26159ec384682af5488a
Satch Puri <sp1796@g.rit.edu>
2016-11-12 20:34:44 -0500

    Added SoundManager and background Music

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource sfxSource;
+    public AudioSource musicSource;
+    public static SoundManager instance = null;
+
+    public float lowPitch = .95f;
+    public float highPitch = 1.05f;
+	// Use this for initialization
+	void Awake ()
+    {
+	if (instance == null)
+        {
+            instance = this;
+        }
+    else if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+	}
+
+    public void PlayingSingle(AudioClip [] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitch, highPitch);
+
+        sfxSource.pitch = randomPitch;
+        sfxSource.clip = clips[randomIndex];
+        sfxSource.Play();
+
+    }
+	
+	// Update is called once per frame
+
+}
