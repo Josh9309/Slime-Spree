05a691ab308c3f738ced07faf6b5cf1c1c48bff5
Josh Malmquist <joshmalmquist96@gmail.com>
2016-11-13 08:15:33 -0500

    Eletric shot attack is now working

--- Assets/Scripts/ElectricSlimeShot.cs
+++ Assets/Scripts/ElectricSlimeShot.cs
@@ -5,12 +5,13 @@ public class ElectricSlimeShot : SlimeShot {
 
     [SerializeField]
     private float eletricSlimeRadius;
+    private YellowSlimePlayer yellowSlime;
 
     // Use this for initialization
     protected override void Awake()
     {
         base.Awake();
-
+        yellowSlime = GameObject.Find("YellowSlime(Clone)").GetComponent<YellowSlimePlayer>();
         damage = 0;
     }
 
@@ -20,6 +21,14 @@ public class ElectricSlimeShot : SlimeShot {
         shotDirection = (slimeShotTarget - transform.position).normalized;
 
         rBody.AddForce(shotDirection * shotSpeed); //Add forces to shoot the slime shot
+
+        if (Mathf.Abs(slimeShotTarget.x - rBody.position.x) / 7 <= 0.05f && Mathf.Abs(slimeShotTarget.y - rBody.position.y) / 7 <= 0.05f)
+        {
+            if (!coroutineRunning)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     protected override void explode()
@@ -35,7 +44,7 @@ public class ElectricSlimeShot : SlimeShot {
                 if (colls[i].transform.tag == "Enemy")
                 {
 
-                    StartCoroutine(colls[i].transform.GetComponent<Enemy>().EletrocuteEnemy(this.gameObject));
+                    StartCoroutine(colls[i].transform.GetComponent<Enemy>().EletrocuteEnemy(this.gameObject, yellowSlime.SlimeAttack2Damage));
                     coroutineRunning = true;
                 }
             }
@@ -50,6 +59,10 @@ public class ElectricSlimeShot : SlimeShot {
     {
         if (coll.gameObject.tag == "Enemy") //If the slime is colliding with the enemy
         {
+            rBody.constraints = RigidbodyConstraints2D.FreezeAll;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<Collider2D>().enabled = false;
+
             Enemy enemySlime = coll.gameObject.GetComponent<Enemy>();
             if (enemySlime.SlimeType != slimePlayerScript.SlimerType)
             {
@@ -62,7 +75,7 @@ public class ElectricSlimeShot : SlimeShot {
                     if (colls[i].transform.tag == "Enemy")
                     {
 
-                        StartCoroutine(colls[i].transform.GetComponent<Enemy>().EletrocuteEnemy(this.gameObject));
+                        StartCoroutine(colls[i].transform.GetComponent<Enemy>().EletrocuteEnemy(this.gameObject, yellowSlime.SlimeAttack2Damage));
                         coroutineRunning = true;
                     }
                 }
@@ -71,6 +84,14 @@ public class ElectricSlimeShot : SlimeShot {
                     Destroy(gameObject);
                 }
             }
+        }
+        else if(coll.gameObject == yellowSlime.gameObject || coll.tag == "Player Detector")
+        {
+            
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 }
